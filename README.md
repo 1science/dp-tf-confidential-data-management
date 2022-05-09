79d8e57f781e1d9e15e4256ee6330ac1c1654de4
Dominic Roberge <dominic.roberge.11@gmail.com>
2022-05-09 15:21:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# dp-tf-confidential-data-management
+
+Repo for the terraform config for s3 access management.
+
+To be continued.
---
+++ terraform/s3.tf
@@ -0,0 +1,20 @@
+
+resource "aws_s3_bucket" "dataconfidential" {
+  bucket   = "com-elsevier-rdp-dataconfidential-nonprod-useast2-1"
+  provider = aws.us-east-2
+
+  tags = {
+    Name        = "dataconfidential"
+    Environment = "nonprod"
+  }
+}
+
+resource "aws_s3_bucket_policy" "dataconfidential_policy" {
+  bucket = "com-elsevier-rdp-dataconfidential-nonprod-useast2-1"
+  policy = file("configuration/dataconfidential-nonprod-policy.json")
+}
+
+resource "aws_s3_bucket_acl" "dataconfidential_acl" {
+  bucket = aws_s3_bucket.dataconfidential.id
+  acl    = "private"
+}
