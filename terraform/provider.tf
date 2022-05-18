1ed32a945155d8c424125702989eeb5bb68c16c6
Dominic Roberge <dominic.roberge.11@gmail.com>
2022-05-18 10:04:22 -0400

    remove useless stuff

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,14 +8,3 @@ provider "aws" {
     role_arn     = "arn:aws:iam::210275200797:role/ADFS-Developer"
   }
 }
-
-provider "aws" {
-  region                   = "us-east-2"
-  alias                    = "cluster"
-  shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "dp-dev-app"
-  assume_role {
-    session_name = "dp-dev-app"
-    role_arn     = "arn:aws:iam::296075517832:role/ADFS-Developer"
-  }
-}
