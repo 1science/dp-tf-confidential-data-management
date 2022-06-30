0ea4c8e69f6ee323dd895c0c171151f6afefa79e
Dominic Roberge <dominic.roberge.11@gmail.com>
2022-06-30 09:34:03 -0400

    WD-2676: Add role for orcid writer and bucket policy

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -15,6 +15,26 @@ resource "aws_iam_role" "dp_access_transformer" {
   })
 }
 
+resource "aws_iam_role" "dp_access_writer" {
+  provider = aws.bucket
+  name     = "dp-access-writer-1"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Effect" : "Allow"
+        "Principal" : { "AWS" : var.config["dp_access_writer_arn"] }
+        "Action" : "sts:AssumeRole"
+        "Condition" : {}
+      }
+    ]
+  })
+}
+
 output "dp_access_transformer_arn" {
   value = aws_iam_role.dp_access_transformer.arn
+}
+
+output "dp_access_writer_arn" {
+  value = aws_iam_role.dp_access_writer.arn
 }
