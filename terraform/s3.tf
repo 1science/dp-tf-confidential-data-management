62fac16d4af0d936d5d1f04f9966a7ae1f3a4418
davkuyek <d.kuyek+copilot@elsevier.com>
2023-11-08 11:12:20 -0500

    update policies

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,7 +2,6 @@
 resource "aws_s3_bucket" "dataconfidential" {
   bucket   = var.config["bucket"]
   provider = aws.bucket
-  arn      = var.config["bucket_arn"]
 
   tags = {
     Name        = "dataconfidential"
